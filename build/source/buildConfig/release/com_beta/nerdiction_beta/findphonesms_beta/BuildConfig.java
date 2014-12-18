967ee104740314a501f9af65b28ee120bdc28d77
Bill Ryan <bill@nerdiction.com>
2014-12-18 15:54:09 -0500

    Initial commit yo

---
+++ build/source/buildConfig/release/com_beta/nerdiction_beta/findphonesms_beta/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com_beta.nerdiction_beta.findphonesms_beta;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+}
